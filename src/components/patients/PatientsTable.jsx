754e0108ce85e1bbe2a5579ed8882593472831d6
gflexx <glennenri24@gmail.com>
2023-04-25 21:15:01 +0300

    Updated patients table

--- src/components/patients/PatientsTable.jsx
+++ src/components/patients/PatientsTable.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function PatientsTable(props) {
     return (
-        <div>PatientsTable</div>
+        <div className='flex justify-center'>
+            <div className="w-10/12 md:w-8/12 lg:w-6/12">
+                <div className="">
+                    <div className='overflow-x-auto mt-4'>
+                        <table className="table table-compact table-zebra w-full">
+                            <thead className=''>
+                                <tr>
+                                    <th></th>
+                                    <th>Full Names</th>
+                                    <th>Age</th>
+                                    <th>BMI Status</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/PatientListingPage.jsx
+++ src/pages/PatientListingPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import {IoPeople} from 'react-icons/io5'
+import PatientsTable from '../components/patients/PatientsTable'
 
 function PatientListingPage() {
     return (
@@ -7,7 +9,14 @@ function PatientListingPage() {
             <Helmet>
                 <title>Patient Listing</title>
             </Helmet>
-            <div>Patient Listing</div>
+            <div className='p-3'>
+                <div className="flex justify-center">
+                    <h1 className='text-3xl mb-6 text-primary flex items-center'><IoPeople className='h-10 mr-3'/> Patient Listing</h1>
+                </div>
+                <div>
+                    <PatientsTable/>
+                </div>
+            </div>
         </HelmetProvider>
     )
 }
